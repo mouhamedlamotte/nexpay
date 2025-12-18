5e033dd2e174987f416b6a7f03e4c8f74ea35036
mouhamedlamotte <mouhamedlamotte.dev@gmail.com>
2025-12-18 21:59:31 +0000

    fix : add port to local url

--- api/src/config/app.config.ts
+++ api/src/config/app.config.ts
@@ -8,7 +8,7 @@ export default registerAs('app', () => ({
   domain: process.env.APP_DOMAIN,
   globalPrefix: process.env.GLOBAL_PREFIX,
   url:
-    `${process.env.NODE_ENV === 'production' ? 'https' : 'http'}://${process.env.APP_DOMAIN}` ||
+    `${process.env.NODE_ENV === 'production' ? 'https' : 'http'}://${process.env.APP_DOMAIN}:${process.env.APP_PORT}` ||
     'http://localhost:9000',
 
   security: {
